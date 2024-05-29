48ff5baa693f9ebda5cef0807ef80977801b93d9
Muhammad Musab Nasri <156845360+MMusab01@users.noreply.github.com>
2024-05-29 11:16:22 -0700

    Update solution_01.ts

--- solution_01/solution_01.ts
+++ solution_01/solution_01.ts
@@ -1,1 +1,54 @@
+/*
 
+Part 1: Employee Data
+Challenge:
+1. Design a type alias named timpinywe to represent an employee object with properties like nane (string), department (string), and so (string).
+2. Include an optional nested object named contacts to hold phone and email information (if provided)
+3. Employ a union type for the is property to restrict it to "Manager", "Engineer", or "Intern"
+4. Make the skills property an optional array of strings for listing an employee's skills (if any)
+
+*/
+
+//object
+
+//task#1
+type Employees={
+    name:string;
+    department:string;
+    contact?:{
+      phone:number;
+      email:string;
+    };
+    role:"Manager"|"Engineer"|"inter";
+    skills:string[]
+  }
+  
+  
+  let employees :Employees={
+       
+       name:"Musab",
+       
+       department:"Record management",
+       
+       
+       role: "Manager",
+       skills:["inventory","indexing","scanning","client services"]
+       
+  };
+  let employees1= {
+    name:"Abdullah",
+       
+    department:"Hr",
+    
+    
+    role: "Manager",
+    skills:["manage employee data","data entry","making salary","client services"],
+    
+    contact:{
+       
+      Phone:+923000112233,
+    email:"name123@gmail.com",
+    }
+    
+  };
+  
